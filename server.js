a343870cec4f1ec54a459dd93e8474bef5b9cc88
Cypress Frankenfeld <iamcypress@gmail.com>
2013-05-03 17:22:06 -0500

    now uses express

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var http = require("http");
+var express = require('express');
 var config = require("./config");
 var api_key = config.stripe_api_key;
 var stripe = require('stripe')(api_key);
+var app = express();
 
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}
+app.get('/', function(req, res){
+    var body = 'Hello World';
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', body.length);
+    res.end(body);
+});
 
-http.createServer(onRequest).listen(8888);
-
+app.listen(8888);
+console.log('Listening on port 8888');
 
 function create_stripe_customer() {
     stripe.customers.create(

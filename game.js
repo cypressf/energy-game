7d58bed03856e5ed7e98a7f3383bf56986eff5ae
Cypress Frankenfeld <iamcypress@gmail.com>
2013-05-03 15:57:45 -0500

    added styles and a purchase button

--- game.js
+++ game.js
@@ -1,43 +1,50 @@
 // closure to encapsulate variables
 var game = (function(){
-    var points = 0;
-    var point_grow_rate = 1;
+    var energy = 0;
+    var energy_grow_rate = 1;
 
-    // you get points for inviting friends, and for clicking a button that says
+    // you get energy for inviting friends, and for clicking a button that says
     // "I like this game"
-    var invite_point_bonus = 2000;
-    var friend_accepts_point_bonus = 5000;
-    var like_point_bonus = 500;
+    var invite_energy_bonus = 2000;
+    var friend_accepts_energy_bonus = 5000;
+    var like_energy_bonus = 500;
+    var purchase_bonus = 10000;
 
     var intervalID;
 
     var invite_button = document.querySelector("#invite");
     var like_button = document.querySelector("#like");
+    var purchase_button = document.querySelector("#purchase");
 
-    // run this every so often to give the player more points
-    var grow_points = function() {
-        points += point_grow_rate;
-        updateDOM();
+    // run this every so often to give the player more energy
+    var grow_energy = function() {
+        update_energy(1);
     };
 
     var add_like_bonus = function() {
-        points += like_point_bonus;
-        updateDOM();
+        update_energy(like_energy_bonus);
     }
 
     var add_invite_bonus = function() {
-        points += invite_point_bonus;
-        updateDOM();
+        update_energy(invite_energy_bonus);
     }
 
     var add_friend_accepts_bonus = function() {
-        points += friend_accepts_point_bonus;
-        updateDOM();
+        update_energy(friend_accepts_energy_bonus);
+    }
+
+    var add_purchase_bonus = function() {
+        update_energy(purchase_bonus);
     }
     
+    var update_energy = function(amount) {
+        energy += amount;
+        updateDOM();
+    }
+
     var start_counter = function() {
         if (!intervalID) {
-            intervalID = setInterval(grow_points, 1000);
+            intervalID = setInterval(grow_energy, 1000 / energy_grow_rate);
         }
     }
 
@@ -48,12 +55,20 @@ var game = (function(){
         }
     }
     var updateDOM = function() {
-        document.querySelector("#points").innerText = points;
+        document.querySelector("#energy").innerText = energy;
+    }
+
+    var set_grow_rate = function(rate) {
+        energy_grow_rate = rate;
+        stop_counter();
+        start_counter();
     }
 
     invite_button.addEventListener("click", add_invite_bonus);
     like_button.addEventListener("click", add_like_bonus);
+    purchase_button.addEventListener("click", add_purchase_bonus);
 
     start_counter();
 
+    return {set_grow_rate: set_grow_rate};
 })()

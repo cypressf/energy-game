26ce746e9dfd0034df6dcdbb613535e3f37731ad
Cypress Frankenfeld <iamcypress@gmail.com>
2013-05-04 09:31:45 -0500

    trying out passport for logging in to facebook (broken)

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 var express = require('express');
 var redis = require('redis');
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
 var config = require("./config");
 var stripe = require('stripe')(config.stripe_api_key);
 // var db = redis.createClient(6379, 'nodejitsudb7334094310.redis.irstack.com');
 var app = express();
 
+passport.use(new FacebookStrategy({
+    clientID: 529998473723265,
+    clientSecret: config.facebook_api_key,
+    callbackURL: "http://localhost:8888/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done);
+    // User.findOrCreate(..., function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+  }
+));
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'publish_actions' }));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/' }));
 // db.auth(config.redis_api_key, function(err) {
 //     if (err) {
 //         throw err;

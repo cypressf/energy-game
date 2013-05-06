fd885eda195436b3f50123759e4987391b68883a
Cypress Frankenfeld <iamcypress@gmail.com>
2013-05-06 03:08:28 -0500

    session in socket.io half-working

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express =     require('express'),
     http =        require('http'),
     db =          require('redis-url').connect(process.env.REDISTOGO_URL),
-    RedisStore =  require('connect-redis')(express),
+    redis_store = require('connect-redis')(express),
+    io_session =  require('socket.io-session'),
     path =        require('path'),
     stripe =      require('stripe')(process.env['STRIPE_SECRET_DEV']),
     io =          require('socket.io');
@@ -9,13 +10,14 @@ var express =     require('express'),
 var app = express(),
     server = http.createServer(app),
     io = io.listen(server),
+    session_store = new redis_store({client:db});
     port = process.env.PORT || 5000;
 
 app.configure(function () {
-    app.use(express.logger());
+    // app.use(express.logger());
     app.use(express.bodyParser());
-    app.use(express.cookieParser('secret thing goes here'));
-    app.use(express.session());
+    app.use(express.cookieParser());
+    app.use(express.session({secret: 'secret', store: session_store, cookie: { maxAge: 1000*60*60*14 }}));
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -23,7 +25,15 @@ io.configure(function () {
     // heroku doesn't support websockets, so we need to use longpolling
     // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
     io.set("transports", ["xhr-polling"]); 
-    io.set("polling duration", 10); 
+    io.set("polling duration", 10);
+
+    // reduce debug logging
+    // http://stackoverflow.com/questions/6807775/socket-io-remove-debug-mode
+    io.set('log level', 1);
+
+    // give socket.io access to express sessions
+    // https://npmjs.org/package/socket.io-session
+    io.set('authorization', io_session(express.cookieParser('secret'), session_store)); 
 });
 
 
@@ -53,5 +63,18 @@ server.listen(port, function(){
 });
 
 io.sockets.on('connection', function(socket) {
-    io.sockets.emit('message', {"message": "new person is connected"});
+    var session = socket.handshake.session;
+    console.log(socket.handshake.sessionId);
+    console.log(session);
+    if (session.name) {
+        socket.emit('update', {"message": "hi " + session.name});
+    }
+    socket.on('update', function(data){
+        if (data.name) {
+            session.name = data.name;
+            session.save();
+        }
+        console.log(session);
+        socket.emit('update', {"message": "received " + session.name});
+    });
 });

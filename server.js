5fc71094a1d45a92b67e0bd4fbedc8ef99742fd5
Cypress Frankenfeld <iamcypress@gmail.com>
2013-05-06 01:32:54 -0500

    removed passport auth dummy code. todo later

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
-// ==============================================
-// Server, framework, socket.io, and database
-// ==============================================
-var express = require('express');
-var app = express();
-var http = require('http');
-var db = require('redis-url').connect(process.env.REDISTOGO_URL);
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-var port = process.env.PORT || 5000;
-var RedisStore = require('connect-redis')(express);
-var path = require('path');
+var express =     require('express'),
+    http =        require('http'),
+    db =          require('redis-url').connect(process.env.REDISTOGO_URL),
+    RedisStore =  require('connect-redis')(express),
+    path =        require('path'),
+    stripe =      require('stripe')(process.env['STRIPE_SECRET_DEV']),
+    io =          require('socket.io');
 
-// ===============================================
-// Authentication, facebook, and payment library
-// ===============================================
-var passport = require('passport')
-var FacebookStrategy = require('passport-facebook').Strategy;
-var stripe_secret = process.env['STRIPE_SECRET_DEV'];
-var facebook_secret = process.env['FACEBOOK_SECRET'];
-var facebook_id = process.env['FACEBOOK_ID'];
-var stripe = require('stripe')(stripe_secret);
+var app = express(),
+    server = http.createServer(app),
+    io = io.listen(server),
+    port = process.env.PORT || 5000;
 
-
-// ===============================================
-// App configuration
-// ===============================================
 app.configure(function () {
     app.use(express.logger());
     app.use(express.bodyParser());
@@ -33,50 +19,16 @@ app.configure(function () {
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
-// heroku doesn't support websockets, so we need to use longpolling : (
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+io.configure(function () {
+    // heroku doesn't support websockets, so we need to use longpolling
+    // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
 });
-
-// set up the authentication
-passport.use(new FacebookStrategy({
-    clientID: facebook_id,
-    clientSecret: facebook_secret,
-    callbackURL: "http://localhost:5000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
-    console.log(done);
-    // User.findOrCreate(..., function(err, user) {
-    //   if (err) { return done(err); }
-    //   done(null, user);
-    // });
-  }
-));
-
-// Redirect the user to Facebook for authentication.  When complete,
-// Facebook will redirect the user back to the application at
-//     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'publish_actions' }));
-
-// Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-app.get('/auth/facebook/callback', 
-  passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/' }));
-
-
 
 
 // ===============================================
 // API
-// Modify and view the internal state of
-// the game via http requests from the client
 // ===============================================
 app.post('/api/payments', function(request, response){
     var charge = {};
